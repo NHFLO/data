23cf0b6b60fec08b474aea7db736585a87e4ea85
Bas des Tombe <bdestombe@gmail.com>
2024-03-18 12:42:07 +0000

    Added wells_tata v1.0.0

---
+++ src/nhflodata/data/mockup/wells_tata/v1.0.0/prepare_tata_data.py
@@ -0,0 +1,36 @@
+from shapely.geometry import Point
+import geopandas as gpd
+import pandas as pd
+
+
+gdf_tata_zout = gpd.GeoDataFrame(pd.DataFrame(
+    data=[
+        ["A1", -180., -120., 1., 1., Point(0, 1)],
+        ["A2", -180., -120., 1., 1., Point(0, 1)],
+        ], 
+    columns=["Name", "botm", "top", "Q_Mm3/a", "Q_m3/d", "geometry"]),
+    crs="EPSG:28992")
+
+# retreives top and botm from the model
+gdf_tata_zoet = gpd.GeoDataFrame(pd.DataFrame(
+    data=[
+        ["B1", 1., 1., Point(0, 1)],
+        ["B2", 1., 1., Point(0, 1)],
+        ], 
+    columns=["Name", "Q_Mm3/a", "Q_m3/d", "geometry"]),
+    crs="EPSG:28992")
+
+gdf_tata_zout.to_file("tata_zoutwaterbronnen.geojson", driver="GeoJSON")
+gdf_tata_zoet.to_file("tata_zoetwaterbronnen.geojson", driver="GeoJSON")
+
+zoet_fp = "/workspaces/NHFLO/models/modelscripts/09pwnmodel2/data/zoetwaterbronnen_tata.shp"
+zout_fp = "/workspaces/NHFLO/models/modelscripts/09pwnmodel2/data/zoutwaterbronnen_tata.shp"
+
+zout = gpd.read_file(zout_fp)[["Name", "botm", "top", "Q_Mm3/a", "Q_m3/d", "geometry"]]
+zoet = gpd.read_file(zoet_fp)[["Name", "Q_Mm3/a", "Q_m3/d", "geometry"]]
+
+zoetzout = pd.concat((zoet, zout))
+print(f"Extent: {zoetzout.total_bounds}")
+
+zout.to_file("tata_zoutwaterbronnen_unaltered.geojson", driver="GeoJSON")
+zoet.to_file("tata_zoetwaterbronnen_unaltered.geojson", driver="GeoJSON")
