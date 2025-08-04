d21465247ee80ff029ecdd84499b9958e5110986
Bas des Tombe <bdestombe@gmail.com>
2025-08-04 16:32:33 +0200

    Update drains_pwn script and repository metadata with versioning and details

--- src/nhflodata/data/mockup/drains_pwn/v1.0.0/drains_pwn.py
+++ src/nhflodata/data/mockup/drains_pwn/v1.0.0/drains_pwn.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import geopandas as gpd
-from nhflotools.geoconverter.utils import validate_crs, validate_geometry
+from nhflotools.geoconverter.utils import round_bounds, validate_crs, validate_geometry
 
 from nhflodata import get_abs_data_path
 
@@ -32,3 +32,6 @@ for idx, geom in enumerate(gdf.geometry):
 
 out_geojson = Path(__file__).parent / "drains_pwn.geojson"
 gdf[columns_to_keep].to_file(out_geojson, driver="GeoJSON", write_bbox=True, coordinate_precision=2)
+
+bounds = dict(zip(["minx", "miny", "maxx", "maxy"], gdf.total_bounds, strict=False))
+print("extent:", round_bounds(bounds, rounding_interval=1000.0).values())  # noqa: T201
