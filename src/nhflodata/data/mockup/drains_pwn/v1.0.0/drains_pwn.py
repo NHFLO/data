39ee8f39b9bbf82c50d71472519fca42bb3bd25d
Bas des Tombe <bdestombe@gmail.com>
2025-08-05 14:26:45 +0200

    Enhance drains_pwn script by adding conductance properties and integrating open water drainage features

--- src/nhflodata/data/mockup/drains_pwn/v1.0.0/drains_pwn.py
+++ src/nhflodata/data/mockup/drains_pwn/v1.0.0/drains_pwn.py
@@ -1,26 +1,63 @@
 from pathlib import Path
 
 import geopandas as gpd
+import numpy as np
+import pandas as pd
 from nhflotools.geoconverter.utils import round_bounds, validate_crs, validate_geometry
 
 from nhflodata import get_abs_data_path
 
+columns_to_keep = [
+    "name",
+    "elevation",  # Elevation of the bottom of the drain
+    "conductance_per_meter",
+    "conductance_per_squared_meter",
+    "bgt-identificatie",
+    "mover_lake_name",
+    "comment",
+    "geometry",
+]
+
+# %% Create line drains input
 gdf_dir = get_abs_data_path("drains_en_damwand_bergen")
 gdf = gpd.read_file(gdf_dir / "drains_en_damwand_bergen.geojson")
 gdf = gdf.loc[gdf.naam.str.startswith("drain")]
 gdf["elevation"] = gdf[["diepte_van", "diepte_tot"]].min(axis=1)
 gdf = gdf.rename(columns={"naam": "name", "opmerking": "comment"})
-columns_to_keep = [
-    "name",
-    "elevation",  # Elevation of the bottom of the drain
-    "comment",
-    "geometry",
-]
-gdf = gdf[columns_to_keep]
 
 # Add conductances
 # m3/day per meter head difference per meter drain length
 gdf["conductance_per_meter"] = 1.0  # Default conductance
+gdf["conductance_per_squared_meter"] = np.nan  # Default conductance for open water
+gdf["bgt-identificatie"] = ""  # Placeholder for BGT identification
+gdf["mover_lake_name"] = ""  # Placeholder for mover lake boundary name, to move drained water to.
+gdf = gdf[columns_to_keep]
+
+# %% Add open water drainage
+# m3/day per meter head difference  per squaredd meter
+bergen_oppwater_path = get_abs_data_path("oppervlaktewater_bergen")
+oppwater = gpd.read_file(bergen_oppwater_path / "oppervlaktewater_bergen.geojson")
+
+open_water_drainage = oppwater.loc[oppwater["naam"].eq("bassin")]
+gdf = pd.concat(
+    (
+        gdf,
+        gpd.GeoDataFrame(
+            data={
+                "name": open_water_drainage["naam"],
+                "elevation": open_water_drainage["peil_circa"],
+                "comment": "",
+                "geometry": open_water_drainage["geometry"],
+                "conductance_per_meter": np.nan,  # Default conductance for open water
+                "conductance_per_squared_meter": 1.0,  # Default conductance for open water
+                "bgt-identificatie": "",
+                "mover_lake_name": "vijver 4",
+            }
+        ),
+    ),
+    axis=0,
+    ignore_index=True,
+)
 
 # Checks
 validate_crs(gdf)
