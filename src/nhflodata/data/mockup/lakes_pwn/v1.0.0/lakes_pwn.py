ee744d68c8fed8f534071e74c70e9bbd40250deb
Bas des Tombe <bdestombe@gmail.com>
2025-08-05 14:27:05 +0200

    Add validation checks for geometry and export path adjustment in lakes_pwn script

--- src/nhflodata/data/mockup/lakes_pwn/v1.0.0/lakes_pwn.py
+++ src/nhflodata/data/mockup/lakes_pwn/v1.0.0/lakes_pwn.py
@@ -1,7 +1,8 @@
 """Create Lakes input file for dunes."""
 
+from pathlib import Path
+
 import geopandas as gpd
-import pandas as pd
 from nhflotools.geoconverter.utils import round_bounds, validate_crs, validate_geometry
 from nlmod.gwf.lake import LAKE_KWDS, OUTLET_DEFAULT
 
@@ -110,4 +111,17 @@ print("extent:", round_bounds(bounds, rounding_interval=1000.0).values())  # noq
 
 # Export to sanitized GeoJSON
 gdf_lake = gdf_lake.astype(dtypes)
-gdf_lake.to_file("lakes_pwn.geojson", driver="GeoJSON", write_bbox=True, coordinate_precision=2)
+
+# Checks
+validate_crs(gdf_lake)
+for idx, geom in enumerate(gdf_lake.geometry):
+    is_valid, error = validate_geometry(geom)
+    if not is_valid:
+        msg = f"Geometry at index {idx} is invalid: {error}"
+        raise ValueError(msg)
+
+out_geojson = Path(__file__).parent / "lakes_pwn.geojson"
+gdf_lake.to_file(out_geojson, driver="GeoJSON", write_bbox=True, coordinate_precision=2)
+
+bounds = dict(zip(["minx", "miny", "maxx", "maxy"], gdf_lake.total_bounds, strict=False))
+print("extent:", round_bounds(bounds, rounding_interval=1000.0).values())  # noqa: T201
